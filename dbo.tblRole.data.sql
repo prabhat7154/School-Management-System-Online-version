85ea65abd265ed68253462dd3a31daf25794b2a2
prabhat7154 <prabhat7154@gmail.com>
2019-12-20 16:15:47 +0530

    Add project files.

---
+++ dbo.tblRole.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[tblRole] ([roleId], [roleName], [roleStatus]) VALUES (N'ROL1M1Z4Y3S1B', N'MANAGER', N'Active')
+INSERT INTO [dbo].[tblRole] ([roleId], [roleName], [roleStatus]) VALUES (N'ROL7S7M1V1T0B', N'Admin', N'In-Active')
